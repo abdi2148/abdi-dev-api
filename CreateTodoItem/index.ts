16056293a73913a29e4c334ad20a7db604fd4cb9
abdi2148 <abdiqafar.abas@gmail.com>
2022-12-20 13:02:38 +0100

    upload todo items trigger added 3:33

--- CreateTodoItem/index.ts
+++ CreateTodoItem/index.ts
@@ -1,13 +1,40 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { getGuid, getUserId } from "../Common/Utils";
+import { NewTodoItem } from "../Models/new-todo-item";
+import { TodoItem } from "../Models/todo-item";
+import { TodoItemRecord } from "../Models/todo-item-record";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-   
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: req.body
-    };
+    context.log.info('Create todo item started.');
 
+    const newItem = req.body as NewTodoItem;
+
+    if(newItem && newItem.title && newItem.description){
+        const todoItem = {
+            id: getGuid(),
+            title: newItem.title,
+            description: newItem.description
+        } as TodoItem;
+
+        const todoItemRecord = {
+            userId: getUserId(),
+            ...todoItem
+        } as TodoItemRecord;
+
+        context.bindings.todoItemRecord = todoItemRecord;
+
+        context.res = {
+            status:201,
+            body: todoItem
+        };
+    } else {
+        context.res = {
+            status: 400
+        };
+        context.log.error('Create todo item invalid input.', context.invocationId, JSON.stringify(newItem));
+    }
+    context.log.info('Create todo item completed.');
+    context.done();
 };
 
 export default httpTrigger;
